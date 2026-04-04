70b3c3bd3a02d9f0ded879fbfd47f83dcb95686a
Pranay Kadam <78108370+xXpsyXx@users.noreply.github.com>
2026-04-04 12:38:24 +0530

    Add PKCS#11 initialization and configuration in main module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DscModule } from '../modules/dsc/dsc.module';
+import { Pkcs11ConfigService } from './config/pkcs11.config';
 
 @Module({
   imports: [DscModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Pkcs11ConfigService],
 })
 export class AppModule {}
